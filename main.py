611512c2306084c5fa30b6e48a911bafb18d6bb9
Hritwik NE <hritwikdileep@outlook.com>
2022-11-27 16:50:54 +0530

    Modified title in main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ with app_view:
         choice = st.radio('Are you uploading an image cropped to the word?', ["Yes, don't crop", "No, crop it"])
         crop = False if choice=="Yes, don't crop" else True
 
-    st.info("Upload a cropped word image for better prediction accuracy")
+    st.info("Upload a cropped word image, rather than non-cropped, for better prediction accuracy")
     uploadTab, takePictureTab = st.tabs(["Upload", "Take a picture"])
 
     with uploadTab:
@@ -59,14 +59,14 @@ with app_view:
 
             
         if st.session_state["Process"]:
-            if st.button("Correct"):
+            if st.button("Correct", key=10):
                 st.session_state["Correct"] = True
-            if st.button("Incorrect"):
+            if st.button("Incorrect", key=11):
                 st.session_state["Incorrect"] = True
 
         if st.session_state["Correct"]:
             st.balloons()
-            st.write("I'm getting better than humans :smirk:")
+            st.write("Am I getting better than humans :smirk:")
             st.session_state["Correct"] = False
             st.session_state["Process"] = False
             
@@ -83,12 +83,35 @@ with app_view:
 
         displayOriginal = st.checkbox('Show the cropped input word image along with the result', key=2)
         st.markdown(" "); st.markdown(" ")
+        if "Process" not in st.session_state:
+            st.session_state["Process"] = False
+        if "Correct" not in st.session_state:
+            st.session_state["Correct"] = False
+        if "Incorrect" not in st.session_state:
+            st.session_state["Incorrect"] = False
+
         process = st.button('Process', key=4)
-        
         if process:
             file = [file]
             predict_menu(file, process, crop, language, displayOriginal, True) 
+            st.session_state["Process"] = True
             
+        if st.session_state["Process"]:
+            if st.button("Correct", key=12):
+                st.session_state["Correct"] = True
+            if st.button("Incorrect", key=13):
+                st.session_state["Incorrect"] = True
+
+        if st.session_state["Correct"]:
+            st.balloons()
+            st.write("Am I getting better than humans :smirk:")
+            st.session_state["Correct"] = False
+            st.session_state["Process"] = False
+            
+        if st.session_state["Incorrect"]:
+            st.write("Sorry, maybe I should train more :confounded:")
+            st.session_state["Incorrect"] = False
+            st.session_state["Process"] = False
 
 
 with demo_view:
