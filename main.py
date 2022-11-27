8309aab11702dfe63dc992cbaa62140e0bdc17fe
Hritwik NE <hritwikdileep@outlook.com>
2022-11-27 13:14:00 +0530

    Modified title in main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ with st.sidebar:
 # Main headers
 st.markdown('''
     # Netram  
+      
     **What does this app do?**  
     Given the image of a (isolated) word in a natural scene (like billboards, banners), 
     the app predicts the text in it. This problem is formally stated as **Scene Text Recognition**.  
