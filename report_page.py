bb0a472f978945356a0f3bb369a216e950730750
Hritwik NE <hritwikdileep@outlook.com>
2022-12-18 09:36:51 +0530

    Fixed links in references section

--- report_page.py
+++ report_page.py
@@ -10,7 +10,7 @@ def report_page():
 
     st.markdown('''
     ### References  
-    > [*Mathew et al* &nbsp; - &nbsp; Benchmarking Scene Text Recognition in Devanagari, Telugu and Malayalam]("https://cdn.iiit.ac.in/cdn/cvit.iiit.ac.in/images/ConferencePapers/2017/Benchmarkingtelugu_malayalam.pdf")  
-    > [*Tian et al* &nbsp; - &nbsp; Detecting Text in Natural Image with Connectionist Text Proposal Network]("https://arxiv.org/abs/1609.03605")  
+    > [*Mathew et al* &nbsp; - &nbsp; Benchmarking Scene Text Recognition in Devanagari, Telugu and Malayalam](https://cdn.iiit.ac.in/cdn/cvit.iiit.ac.in/images/ConferencePapers/2017/Benchmarkingtelugu_malayalam.pdf)  
+    > [*Tian et al* &nbsp; - &nbsp; Detecting Text in Natural Image with Connectionist Text Proposal Network](https://arxiv.org/abs/1609.03605)  
     ***  
-    ''')
+    ''', unsafe_allow_html=True)
